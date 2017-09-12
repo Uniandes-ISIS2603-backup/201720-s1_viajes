a0c7ac53c39238d265a8ea34a81b208b48095d68
m.rodriguez21 <m.rodriguez21@ISIS2603S1-0024.sis.virtual.uniandes.edu.co>
2017-09-12 16:56:53 -0500

    Revert "Rama master inicial"
    This reverts commit c2c4326b21befb02fcb6aa4e7943fc5b4e3ee002.

---
+++ viajes/viajes-back/src/main/java/co/edu/uniandes/csw/viajes/entities/OficinaEntity.java
@@ -0,0 +1,59 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.uniandes.csw.viajes.entities;
+
+import javax.persistence.FetchType;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+
+/**
+ *
+ * @author m.rodriguez21
+ */
+public class OficinaEntity extends BaseEntity
+{
+    private String nombreLugar;
+    private String nombreEncargado;
+    
+    //////////////////////////////
+    //REVISAR SI SE HACE @EMBEDDED
+    //////////////////////////////
+    @OneToOne (fetch=FetchType.LAZY)
+    @JoinColumn (name="UBICACION_ID") 
+    private UbicacionEntity ubicacion;
+    
+    
+    public String getNombreLugar()
+    {
+        return nombreLugar;
+    }
+    
+    public void setNombreLugar(String nombreLugar)
+    {
+        this.nombreLugar = nombreLugar;
+    }
+    
+    public String getNombreEncargado()
+    {
+        return nombreEncargado;
+    }
+    
+    public void setNombreEncargado(String nombreEncargado)
+    {
+        this.nombreEncargado = nombreEncargado;
+    }
+    
+    public UbicacionEntity getUbicacion()
+    {
+        return ubicacion;
+    }
+    
+    public void setUbicacion(UbicacionEntity ubicacion)
+    {
+        this.ubicacion = ubicacion;
+    }
+
+}
