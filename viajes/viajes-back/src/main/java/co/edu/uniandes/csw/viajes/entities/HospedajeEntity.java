6e08a9b9c273250554babbd6160a743f3abe3aaa
ma.forero11 <ma.forero11@ISIS2603S1-0038.sis.virtual.uniandes.edu.co>
2017-09-12 17:15:30 -0500

    Eliminadas clases que habían quedado en la master por error, para poder hacer pruebas sobre mis clases, eliminadas relaciones con otras clases para quitar errores, añadidos atributos temporalmente mientras se crea servicioEntity

--- viajes/viajes-back/src/main/java/co/edu/uniandes/csw/viajes/entities/HospedajeEntity.java
+++ viajes/viajes-back/src/main/java/co/edu/uniandes/csw/viajes/entities/HospedajeEntity.java
@@ -15,40 +15,113 @@ import javax.persistence.OneToOne;
  *
  * @author ma.forero11
  */
-public class HospedajeEntity extends ServicioEntity{
+public class HospedajeEntity{
+
+    private String fechaInicio;
+    private String fechaFinal;
+    private String nombre;
+    private Double valor;
+    private Integer calificacion;
+    private String comentarios;
+    private long id;
+    private String descripcion;
     
-    private UbicacionEntity ubicacion;
-    private List<ImagenEntity> imagenes;
-    private CompaniaEntity compania;
-    
-     public UbicacionEntity getUbicacion()
-    {
-        return ubicacion;
+    public String getFechaInicio() {
+        return fechaInicio;
     }
+
+    public void setFechaInicio(String fechaInicio) {
+        this.fechaInicio = fechaInicio;
+    }
+
+    public String getFechaFinal() {
+        return fechaFinal;
+    }
+
+    public void setFechaFinal(String fechaFinal) {
+        this.fechaFinal = fechaFinal;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Double getValor() {
+        return valor;
+    }
+
+    public void setValor(Double valor) {
+        this.valor = valor;
+    }
+
+    public Integer getCalificacion() {
+        return calificacion;
+    }
+
+    public void setCalificacion(Integer calificacion) {
+        this.calificacion = calificacion;
+    }
+
+    public String getComentarios() {
+        return comentarios;
+    }
+
+    public void setComentarios(String comentarios) {
+        this.comentarios = comentarios;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+    
+    //private UbicacionEntity ubicacion;
+    //private List<ImagenEntity> imagenes;
+    //private CompaniaEntity compania;
+    
+    // public UbicacionEntity getUbicacion()
+    //{
+    //    return ubicacion;
+    //}
      
-    public void setUbicacion(UbicacionEntity ubicacion)
-    {
-        this.ubicacion = ubicacion;
-    }
-     
-    public List<ImagenEntity> getImagenes()
-    {
-        return imagenes;
-    }
+//    public void setUbicacion(UbicacionEntity ubicacion)
+//    {
+//        this.ubicacion = ubicacion;
+//    }
+//     
+//    public List<ImagenEntity> getImagenes()
+//    {
+//        return imagenes;
+//    }
+//    
+//    public void setImagenes(List<ImagenEntity> imagenes)
+//    {
+//        this.imagenes = imagenes;
+//    }
     
-    public void setImagenes(List<ImagenEntity> imagenes)
-    {
-        this.imagenes = imagenes;
-    }
-    
-    public CompaniaEntity getCompania()
-    {
-        return compania;
-    }
-    
-    public void setCompania(CompaniaEntity compania)
-    {
-        this.compania = compania;
-    }
+//    public CompaniaEntity getCompania()
+//    {
+//        return compania;
+//    }
+//    
+//    public void setCompania(CompaniaEntity compania)
+//    {
+//        this.compania = compania;
+//    }
      
 }
--- viajes/viajes-back/src/main/java/co/edu/uniandes/csw/viajes/entities/UbicacionEntity.java
+++ viajes/viajes-back/src/main/java/co/edu/uniandes/csw/viajes/entities/UbicacionEntity.java
@@ -17,4 +17,58 @@ import javax.persistence.OneToOne;
  */
 public class UbicacionEntity {
     
+    private String longitud;
+    private String latitud;
+    private String nombre;
+    private String direccion;
+    private String ciudad;
+    private String pais;
+
+    public String getLongitud() {
+        return longitud;
+    }
+
+    public void setLongitud(String longitud) {
+        this.longitud = longitud;
+    }
+
+    public String getLatitud() {
+        return latitud;
+    }
+
+    public void setLatitud(String latitud) {
+        this.latitud = latitud;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getCiudad() {
+        return ciudad;
+    }
+
+    public void setCiudad(String ciudad) {
+        this.ciudad = ciudad;
+    }
+
+    public String getPais() {
+        return pais;
+    }
+
+    public void setPais(String pais) {
+        this.pais = pais;
+    }
 }
