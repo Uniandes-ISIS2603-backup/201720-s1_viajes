26e6b605278faad57c8eb9d1a719bdda1664efd9
TATIANA VANESSA HUERTAS BOLAÐOS <tv.huertas10@sis.virtual.uniandes.edu.co>
2017-09-20 02:29:17 -0500

    D

--- viajes-web/src/main/java/org/netbeans/rest/application/config/ApplicationConfig.java
+++ viajes-web/src/main/java/org/netbeans/rest/application/config/ApplicationConfig.java
@@ -34,7 +34,7 @@ public class ApplicationConfig extends Application {
         resources.add(co.edu.uniandes.csw.viajes.resources.HospedajeResource.class);
         resources.add(co.edu.uniandes.csw.viajes.resources.ImagenResource.class);
         resources.add(co.edu.uniandes.csw.viajes.resources.ItinerarioResource.class);
-        resources.add(co.edu.uniandes.csw.viajes.resources.UbicacionResource.class)
+        resources.add(co.edu.uniandes.csw.viajes.resources.UbicacionResource.class);
     }
     
 }
