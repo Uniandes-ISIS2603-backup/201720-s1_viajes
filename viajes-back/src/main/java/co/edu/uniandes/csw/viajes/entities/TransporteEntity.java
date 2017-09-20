11f73bd1647a7fee624940da0d15bba5b05385ad
TATIANA VANESSA HUERTAS BOLAÐOS <tv.huertas10@sis.virtual.uniandes.edu.co>
2017-09-20 02:42:10 -0500

    Sergio

--- viajes-back/src/main/java/co/edu/uniandes/csw/viajes/entities/TransporteEntity.java
+++ viajes-back/src/main/java/co/edu/uniandes/csw/viajes/entities/TransporteEntity.java
@@ -1,40 +1,3 @@
-<<<<<<< HEAD
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package co.edu.uniandes.csw.viajes.entities;
-
-import javax.persistence.Entity;
-
-/**
- *
- * @author sa.silva1
- * Anotaciones: No debia haber constructor
- */
-@Entity
-public class TransporteEntity extends ServicioEntity{
-    
-    public enum TipoTransporte {
-    
-    AVION,
-    TREN,
-    TAXI
-    
-    }
-    
-    private TipoTransporte tipo;
-    
-    public TipoTransporte getTipo() {
-        return tipo;
-    }
-
-    public void setTipo(TipoTransporte tipo) {
-        this.tipo = tipo;
-    }
-    
-=======
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -115,5 +78,4 @@ public class TransporteEntity extends ServicioEntity{
     
     
     */
->>>>>>> master
 }
