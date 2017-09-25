fcff32af52118a9297569e1536f94f3d941cd648
ma.forero11 <ma.forero11@ISIS2603S1-0038.sis.virtual.uniandes.edu.co>
2017-09-24 19:46:34 -0500

    algo pasa con mis resoruces, me manda un 500 y no aparece el [] al lado de los archivos

--- viajes-web/src/main/java/co/edu/uniandes/csw/viajes/resources/HospedajeResource.java
+++ viajes-web/src/main/java/co/edu/uniandes/csw/viajes/resources/HospedajeResource.java
@@ -6,12 +6,16 @@
 package co.edu.uniandes.csw.viajes.resources;
 
 import co.edu.uniandes.csw.viajes.dtos.HospedajeDTO;
+import co.edu.uniandes.csw.viajes.dtos.HospedajeDetailDTO;
 import co.edu.uniandes.csw.viajes.ejb.HospedajeLogic;
 import co.edu.uniandes.csw.viajes.entities.HospedajeEntity;
+import co.edu.uniandes.csw.viajes.excpetions.BusinessLogicException;
 import java.util.ArrayList;
 import java.util.List;
 import javax.ejb.Stateless;
+import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
@@ -19,27 +23,46 @@ import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.WebApplicationException;
 import javax.xml.ws.http.HTTPException;
 
 /**
  *
  * @author ma.forero11
  */
+@Path("hospedajes")
+@Produces("application/json")
+@Consumes("application/json")
+@RequestScoped
 public class HospedajeResource {
     
     @Inject
     HospedajeLogic hospedajeLogic;
     
+    /**
+     * GET para todas los hospedajes.
+     * http://localhost:8080/viajes-web/api/hospedajes
+     *
+     * @return la lista de todas las hospedajes en objetos json DTO.
+     * @throws BusinessLogicException
+     */
+    @GET
+    public List<HospedajeDetailDTO> getHospedajes() throws BusinessLogicException{
+        return listEntity2DetailDTO(hospedajeLogic.getHospedajes());
+    }
+    
     @GET
     @Path("{id: \\d+}")
-    public HospedajeDTO getHospedaje(@PathParam("id") Long id) throws HTTPException {
+    public HospedajeDetailDTO getHospedaje(@PathParam("id") Long id) throws BusinessLogicException {
         HospedajeEntity toGet = hospedajeLogic.getHospedaje(id);
-        if(toGet==null) throw new HTTPException(404);
-        return new HospedajeDTO(toGet);
+        if(toGet==null){
+            throw new WebApplicationException("El recurso /hospedajes/" + id + " no existe.", 404);
+        }
+        return new HospedajeDetailDTO(toGet);
     }
     
     @POST
-    public HospedajeDTO createHospedaje(HospedajeDTO itinerarioDTO){
+    public HospedajeDTO createHospedaje(HospedajeDTO itinerarioDTO)throws BusinessLogicException{
         HospedajeEntity itinerario = itinerarioDTO.toEntity();
         HospedajeEntity entity = hospedajeLogic.createHospedaje(itinerario);  
         return new HospedajeDTO(entity);
@@ -47,7 +70,7 @@ public class HospedajeResource {
     
     @PUT
     @Path("{id: \\d+}")
-    public HospedajeDTO updateHospedaje(@PathParam("id") Long id, HospedajeDTO dto) {
+    public HospedajeDTO updateHospedaje(@PathParam("id") Long id, HospedajeDTO dto)throws BusinessLogicException {
        HospedajeEntity entity = dto.toEntity();
        entity.setId(id);
        return new HospedajeDTO(hospedajeLogic.updateHospedaje(entity)); 
@@ -55,7 +78,26 @@ public class HospedajeResource {
     
     @DELETE
     @Path("{id: \\d+}")
-    public void deleteHospedaje(@PathParam("id") Long id) {
+    public void deleteHospedaje(@PathParam("id") Long id)throws BusinessLogicException {
        hospedajeLogic.deleteHospedaje(id);
+    }
+    
+    /**
+     *
+     * lista de entidades a DTO.
+     *
+     * Este método convierte una lista de objetos HospedajeEntity a una lista de
+     * objetos HospedajeDetailDTO (json)
+     *
+     * @param entityList corresponde a la lista de hospedajes de tipo Entity
+     * que vamos a convertir a DTO.
+     * @return la lista de hospedaje en forma DTO (json)
+     */
+    private List<HospedajeDetailDTO> listEntity2DetailDTO(List<HospedajeEntity> entityList) {
+        List<HospedajeDetailDTO> list = new ArrayList<>();
+        for (HospedajeEntity entity : entityList) {
+            list.add(new HospedajeDetailDTO(entity));
+        }
+        return list;
     }
 }
--- viajes-web/src/main/java/co/edu/uniandes/csw/viajes/resources/UbicacionResource.java
+++ viajes-web/src/main/java/co/edu/uniandes/csw/viajes/resources/UbicacionResource.java
@@ -11,12 +11,16 @@ package co.edu.uniandes.csw.viajes.resources;
  */
 
 import co.edu.uniandes.csw.viajes.dtos.UbicacionDTO;
+import co.edu.uniandes.csw.viajes.dtos.UbicacionDetailDTO;
 import co.edu.uniandes.csw.viajes.ejb.UbicacionLogic;
 import co.edu.uniandes.csw.viajes.entities.UbicacionEntity;
+import co.edu.uniandes.csw.viajes.excpetions.BusinessLogicException;
 import java.util.ArrayList;
 import java.util.List;
 import javax.ejb.Stateless;
+import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
@@ -24,32 +28,49 @@ import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.WebApplicationException;
 import javax.xml.ws.http.HTTPException;
 
+@Path("ubicaciones")
+@Produces("application/json")
+@Consumes("application/json")
+@RequestScoped
 public class UbicacionResource {
-    
-    
     @Inject
     UbicacionLogic ubicacionLogic;
     
+    /**
+     * GET para todas las ubicaciones.
+     * http://localhost:8080/viajes-web/api/ubicaciones
+     *
+     * @return la lista de todas las ubicaciones en objetos json DTO.
+     * @throws BusinessLogicException
+     */
+    @GET
+    public List<UbicacionDetailDTO> getUbicaciones() throws BusinessLogicException{
+        return listEntity2DetailDTO(ubicacionLogic.getUbicaciones());
+    }
+    
     @GET
     @Path("{id: \\d+}")
-    public UbicacionDTO getUbicacion(@PathParam("id") Long id) throws HTTPException {
+    public UbicacionDetailDTO getUbicacion(@PathParam("id") Long id) throws BusinessLogicException {
         UbicacionEntity toGet = ubicacionLogic.getUbicacion(id);
-        if(toGet==null) throw new HTTPException(404);
-        return new UbicacionDTO(toGet);
+        if(toGet==null){
+        throw new WebApplicationException("El recurso /ubicaciones/" + id + " no existe.", 404);
+        }
+        return new UbicacionDetailDTO(toGet);
     }
     
     @POST
-    public UbicacionDTO createUbicacion(UbicacionDTO itinerarioDTO){
+    public UbicacionDetailDTO createUbicacion(UbicacionDTO itinerarioDTO)throws BusinessLogicException{
         UbicacionEntity itinerario = itinerarioDTO.toEntity();
         UbicacionEntity entity = ubicacionLogic.createUbicacion(itinerario);  
-        return new UbicacionDTO(entity);
+        return new UbicacionDetailDTO(entity);
     }
     
     @PUT
     @Path("{id: \\d+}")
-    public UbicacionDTO updateUbicacion(@PathParam("id") Long id, UbicacionDTO dto) {
+    public UbicacionDTO updateUbicacion(@PathParam("id") Long id, UbicacionDTO dto)throws BusinessLogicException {
        UbicacionEntity entity = dto.toEntity();
        entity.setId(id);
        return new UbicacionDTO(ubicacionLogic.updateUbicacion(entity)); 
@@ -57,7 +78,26 @@ public class UbicacionResource {
     
     @DELETE
     @Path("{id: \\d+}")
-    public void deleteUbicacion(@PathParam("id") Long id) {
+    public void deleteUbicacion(@PathParam("id") Long id) throws BusinessLogicException{
        ubicacionLogic.deleteUbicacion(id);
+    }
+    
+    /**
+     *
+     * lista de entidades a DTO.
+     *
+     * Este método convierte una lista de objetos UbicacionEntity a una lista de
+     * objetos UbicacionDetailDTO (json)
+     *
+     * @param entityList corresponde a la lista de ubicaciones de tipo Entity
+     * que vamos a convertir a DTO.
+     * @return la lista de ubicaciones en forma DTO (json)
+     */
+    private List<UbicacionDetailDTO> listEntity2DetailDTO(List<UbicacionEntity> entityList) {
+        List<UbicacionDetailDTO> list = new ArrayList<>();
+        for (UbicacionEntity entity : entityList) {
+            list.add(new UbicacionDetailDTO(entity));
+        }
+        return list;
     }
 }
