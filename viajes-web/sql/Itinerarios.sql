a297eb122b4d091d759dadb04cbdf40a097738dd
JHOAN SEBASTIAN BELTRAN MORALES <js.beltran14@sis.virtual.uniandes.edu.co>
2017-09-25 19:04:49 -0500

    sql

--- viajes-web/sql/Itinerarios.sql
+++ viajes-web/sql/Itinerarios.sql
@@ -8,7 +8,13 @@
  * Created: 19/09/2017
  */
 
+delete from ServicioEntity;
 delete from ItinerarioEntity;
+
 
 insert into ItinerarioEntity (ID, FECHAINICIAL, FECHAFINAL, COSTOTOTAL, NUMEROVISITANTES) values (1,'01/01/17','02/02/17',1000,1);
 insert into ItinerarioEntity (ID, FECHAINICIAL, FECHAFINAL, COSTOTOTAL, NUMEROVISITANTES) values (2,'02/02/17','03/03/17',2000,2);
+
+
+insert into ServicioEntity (ID, CALIFICACION, COMENTARIOS, DESCRIPCION, FECHAFINAL, FECHAINICIO, NOMBRE, VALOR, ITINERARIO_ID) 
+values (1,5,'HOLA','HOLA','02/02/17','01/01/17','HOTEL',1000,1);
