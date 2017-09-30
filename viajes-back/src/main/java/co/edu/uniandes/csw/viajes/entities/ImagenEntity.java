320f998647797a8e7fa5e0554892745737377e74
TATIANA VANESSA HUERTAS BOLAÐOS <tv.huertas10@sis.virtual.uniandes.edu.co>
2017-09-30 15:42:40 -0500

    Prueba

--- viajes-back/src/main/java/co/edu/uniandes/csw/viajes/entities/ImagenEntity.java
+++ viajes-back/src/main/java/co/edu/uniandes/csw/viajes/entities/ImagenEntity.java
@@ -47,4 +47,17 @@ public class ImagenEntity extends BaseEntity{
     public void setComentario(String comentario) {
         this.comentario = comentario;
     }
+    
+    /**
+     * Overide 
+     * @param obj
+     * @return True or false
+     */
+    @Override
+    public boolean equals(Object obj) {
+        if (this.getId() != null && ((ImagenEntity) obj).getId() != null) {
+            return this.getId().equals(((ImagenEntity) obj).getId());
+        }
+        return super.equals(obj);
+    }
 }
