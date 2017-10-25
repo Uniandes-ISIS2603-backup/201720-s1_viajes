39dc6312e11576ded89c743036d4c23e64744bb4
TATIANA VANESSA HUERTAS BOLAÐOS <tv.huertas10@sis.virtual.uniandes.edu.co>
2017-10-25 09:21:02 -0500

    App

--- viajes-web/src/main/webapp/src/app.js
+++ viajes-web/src/main/webapp/src/app.js
@@ -7,33 +7,13 @@
     var app = angular.module('mainApp', [
         // External dependencies
         'ui.router',
+        'ui.bootstrap',
        
         // Internal modules dependencies       
         'hospedajesModule',
         'itinerariosModule',
-       'transportesModule'
-
-    ]);
-    // Resuelve problemas de las promesas
-    app.config(['$qProvider', function ($qProvider) {
-            $qProvider.errorOnUnhandledRejections(false);
-        }]);
-})(window.angular);
-
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-(function (ng) {
-    var app = angular.module('mainApp', [
-        // External dependencies
-        'ui.router',
-        'ui.bootstrap',
-       
-        // Internal modules dependencies       
-       'entretenimientosModule'
-        
+        'transportesModule',
+        'entretenimientosModule'
 
     ]);
     // Resuelve problemas de las promesas
