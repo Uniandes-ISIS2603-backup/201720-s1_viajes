84e9389c62ff71cd906b3a6169874175792d1c42
SERGIO ARTURO SILVA GONZALEZ <sa.silva1@sis.virtual.uniandes.edu.co>
2017-10-24 20:37:08 -0500

    esperando carpeta de modulos

---
+++ viajes-web/src/main/java/mientras/transportes.mod.js
@@ -0,0 +1,21 @@
+(function (ng) {
+var mod = ng.module("transportesModule", []);
+    mod.constant("transportesContext", "api/transportes");
+    mod.config(['$stateProvider', '$urlRouterProvider', function ($stateProvider, $urlRouterProvider) {
+            var basePath = 'src/modules/transportes/';
+            $urlRouterProvider.otherwise("/transportesList");
+
+            $stateProvider.state('transportesList', {
+                url: '/transportes',
+                views: {
+                    'mainView': {
+                        controller: 'transportesCtrl',
+                        controllerAs: 'ctrl',
+                        templateUrl: basePath + 'transportes.list.html'
+                    }
+                }
+            });
+        }]);
+
+})(window.angular);
+
